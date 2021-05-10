62e2b988a6a2d1f3a10e0d516eda83e96088a7de
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-05-10 21:44:54 +0100

    Added usage for insertion sort algorithm

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/itssadon/data-structures-and-algorithms/arrays"
+)
+
+func main() {
+	fmt.Println("This is the main entrance of the program")
+
+	// Arrays
+	insertionSort := arrays.InsertionSort()
+	fmt.Println(insertionSort)
+}
