70d77c19bd8d16fce1b8b61c4c1a3740ba2f769d
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-07-25 23:26:47 +0100

    Tested binary search insertion and searching

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"reflect"
+	"fmt"
 
-	"github.com/itssadon/data-structures-and-algorithms/arrays"
+	"github.com/itssadon/data-structures-and-algorithms/trees"
 )
 
 func main() {
@@ -16,8 +16,15 @@ func main() {
 	// arrayToMerge := []int{1, 2, 4, 8, 10, 3, 5, 7, 9, 11}
 	// arrays.Merge(arrayToMerge, 0, 4, 9)
 
-	unsortedArray := []int{9, 6, 5, 0, 8, 2}
-	size := reflect.TypeOf(unsortedArray).Size() / reflect.TypeOf(unsortedArray[0]).Size()
-	arrays.MergeSort(unsortedArray, 0, int(size-1))
-	arrays.Display(unsortedArray, int(size))
+	// unsortedArray := []int{9, 6, 5, 0, 8, 2}
+	// size := reflect.TypeOf(unsortedArray).Size() / reflect.TypeOf(unsortedArray[0]).Size()
+	// arrays.MergeSort(unsortedArray, 0, int(size-1))
+	// arrays.Display(unsortedArray, int(size))
+
+	/*
+		Trees
+	*/
+	tree := &trees.Node{Key: 100}
+	tree.Insert(2000)
+	fmt.Println(tree)
 }
