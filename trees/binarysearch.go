347283462f3aee68069024e3817f8010aa548d10
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-07-25 23:26:05 +0100

    Added binary search tree file

---
+++ trees/binarysearch.go
@@ -0,0 +1,46 @@
+package trees
+
+// Node: represents the components of a binary search tree
+type Node struct {
+	Key   int
+	Left  *Node
+	Right *Node
+}
+
+// Insert: will add a node to the tree
+// the key to add should not already be in the tree
+func (n *Node) Insert(k int) {
+	if n.Key < k {
+		// move right
+		if n.Right == nil {
+			n.Right = &Node{Key: k}
+		} else {
+			n.Right.Insert(k)
+		}
+	} else if n.Key > k {
+		// move left
+		if n.Left == nil {
+			n.Left = &Node{Key: k}
+		} else {
+			n.Left.Insert(k)
+		}
+	}
+}
+
+// Search: will take in a key value
+// and return true if there is a node with that value
+func (n *Node) Search(k int) bool {
+	if n == nil {
+		return false
+	}
+
+	if n.Key < k {
+		// move right
+		return n.Right.Search(k)
+	} else if n.Key > k {
+		// move left
+		return n.Left.Search(k)
+	}
+
+	return true
+}
